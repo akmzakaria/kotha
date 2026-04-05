6c892b62b839ddca1ce978d6ab427291815bf7ef
akmzakaria <akmzakaria488@gmail.com>
2026-04-06 03:07:11 +0600

    caching fix2

--- app/chat/[chatId]/page.tsx
+++ app/chat/[chatId]/page.tsx
@@ -390,7 +390,7 @@ export default function ChatPage() {
     setShowChatMenu(false)
   }
 
-  const showSkeleton = false
+  const showSkeleton = messages.length === 0 && !otherUserName
 
   return (
     <div
